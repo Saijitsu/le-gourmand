8530d7c0b47c105807b8853af344cedee8717f9c
Nicolas Turlet <35563943+Saijitsu@users.noreply.github.com>
2018-11-22 16:16:40 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MapViewComponent } from './map-view/map-view.component';
+import { RestaurantListComponent } from './restaurant-view/restaurant-list/restaurant-list.component';
+import { RestaurantReviewsComponent } from './restaurant-view/restaurant-reviews/restaurant-reviews.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MapViewComponent,
+    RestaurantListComponent,
+    RestaurantReviewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
---
+++ src/app/restaurant-view/restaurant-reviews/restaurant-reviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-reviews',
+  templateUrl: './restaurant-reviews.component.html',
+  styleUrls: ['./restaurant-reviews.component.scss']
+})
+export class RestaurantReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
