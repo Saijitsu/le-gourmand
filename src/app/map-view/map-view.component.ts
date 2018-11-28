eccc5caa68bd7cd8457c779a737ced03805ec0aa
Nicolas Turlet <35563943+Saijitsu@users.noreply.github.com>
2018-11-28 13:37:25 +0100

    google map: current location

--- src/app/map-view/map-view.component.ts
+++ src/app/map-view/map-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-map-view',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-view.component.scss']
 })
 export class MapViewComponent implements OnInit {
-  title: string = 'My location';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
-  constructor() { }
+  title: String = 'Vous vous trouvez ici!';
+  lat: any;
+  lng: any;
+  constructor() {
+    if (navigator) {
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.lng = +pos.coords.longitude;
+        this.lat = +pos.coords.latitude;
+      });
+    }
+  }
 
   ngOnInit() {
   }
